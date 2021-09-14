601320bb7d79b50cf8db092d6b0eb21eaa44c24b
Aros_zi34@student.itstep.org <Aros_zi34@student.itstep.org>
2021-09-14 21:27:33 +0300

    Final

--- MultiEditor/Editor.h
+++ MultiEditor/Editor.h
@@ -93,6 +93,8 @@ public:
 
 	virtual unique_ptr<IFile> runEditor() = 0;
 
+	void Menu();
+
 private:
 
 };
@@ -129,3 +131,67 @@ public:
 private:
 
 };
+
+void MultiEditorFactory::Menu()
+{
+	unique_ptr<IFile> file;
+	MultiEditorFactory* meditor;
+
+	cout << "1. Текстовый редактор \n2. Графический редактор \n0. Выход\n";
+
+	while (true)
+	{
+
+		int i;
+		cin >> i;
+
+		switch (i)
+		{
+		case 1:
+		{
+			TextFactory tf;
+
+			meditor = &tf;
+
+			file = meditor->runEditor();
+
+			file->printFile();
+
+			file->SaveFile("Дано новое имя для текстового файла при сохранении\n");
+
+			file->printFile();
+
+			cout << endl;
+			break;
+		}
+		case 2:
+		{
+			PictureFactory gf;
+
+			meditor = &gf;
+
+			file = meditor->runEditor();
+
+			file->SetFileName("Graphic File");
+
+			file->SaveFile();
+
+			file->printFile();
+
+			gf.ShowPalette();
+
+			cout << endl;
+			break;
+		}
+
+		case 0:
+		{
+			cout << "Выход..." << endl;
+			exit(0);
+		}
+
+		default:
+			break;
+		}
+	}
+}
--- MultiEditor/Main.cpp
+++ MultiEditor/Main.cpp
@@ -11,72 +11,9 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	unique_ptr<MultiEditorFactory> m;
 
-	unique_ptr<IFile> file;
-
-	//unique_ptr<MultiEditorFactory> meditor; - Почему так нельзя?
-
-	MultiEditorFactory* meditor;
-
-	
-	cout << "1. Текстовый редактор \n2. Графический редактор \n0. Выход\n";
-
-
-	while (true)
-	{
-		
-		int i;
-		cin >> i;
-
-		switch (i)
-		{	
-		case 1:
-		{
-			TextFactory tf;
-
-			meditor = &tf;
-
-			file = meditor->runEditor();
-
-			file->printFile();
-
-			file->SaveFile("Дано новое имя для текстового файла при сохранении\n");
-
-			file->printFile();
-
-			cout << endl;
-			break;
-		}
-		case 2:
-		{
-			PictureFactory gf;
-
-			meditor = &gf;
-
-			file = meditor->runEditor();
-
-			file->SetFileName("Graphic File");
-
-			file->SaveFile();
-
-			file->printFile();
-
-			gf.ShowPalette();
-
-			cout << endl;
-			break;
-		}
-
-		case 0:
-		{
-			cout << "Выход..." << endl; 
-			exit(0);
-		}
-
-		default:
-			break;
-		}
-	}
+	m->Menu();
 	
 
 	cout << endl;
